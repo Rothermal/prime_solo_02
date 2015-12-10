79b74d98d6d9f05beb66333396ce1ff78a7ba783
Mark Rothermal <rothermal2@comcast.net>
2015-12-10 13:01:40 -0600

    initial commit

--- solo_script.js
+++ solo_script.js
@@ -1,13 +1,20 @@
 // ! ! !
 // Three Bugs
+// zero is a test subject. i inserted zero in front of atticus in the 2d array because i wanted to prove that code is getting caught up on the first index of the employee arrays. now to find the bug in the for loop.
+// added [i] to array inside calculateSTI call. now the for loop functions properly. bug 1 found. note, now it builds array but all the salarys are negative, time to check the math.
+// basepercent was set to minus 1 changed + 1.  now  salarys no longer come up negative. but now everyone is getting a 13 percent bonus. did some testing, found the plus 1 to be unnessesary. removed it. numbers seem to be correct, still one bug left.
+// found if employee has a review of 2, and a 4 digit employee number they still got a 5 percent raise, added argument reviewScore to the getYearAdjustment function. 
 
+
+var zero = ["zero","1444","20000",2];
 var arrayAtticus = ["Atticus", "2405", "47000", 3];
 var arrayJem = ["Jem", "62347", "63500", 4];
 var arrayBoo = ["Boo", "11435", "54000", 3];
 var arrayScout = ["Scout", "6243", "74750", 5];
 
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
-
+var array = [zero,arrayAtticus, arrayJem, arrayBoo, arrayScout];
+//verify that array is working correctly.
+console.log(array);
 //Create variables used to write to the DOM
 var newEl, newText, position;
 //Capture the position of insertion into the DOM
@@ -15,28 +22,34 @@ position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
+
+
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
- 	newEl = document.createElement('li');
-	newText = document.createTextNode(array[i]);
-	newEl.appendChild(newText);
-	position.appendChild(newEl);
+  array[i] = calculateSTI(array[i]);
+    newEl = document.createElement('li');
+  newText = document.createTextNode(array[i]);
+  newEl.appendChild(newText);
+  position.appendChild(newEl);
+
 }
 
-function calculateSTI(array1){
+function calculateSTI(array){
   var newArray = [];
 
-  newArray[0] = array1[0];
+  newArray[0] = array[0];
 
-  var employeeNumber = array1[1];
-  var baseSalary = array1[2];
-  var reviewScore = array1[3];
+  var employeeNumber = array[1];
+  var baseSalary = array[2];
+  var reviewScore = array[3];
 
-  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
-  if(bonus > 0.13){
-    bonus = 0.13;
-  }
-
+  
+  var  bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber,reviewScore) - getIncomeAdjustment(baseSalary)  ;
+  
+     if( bonus  > 0.13){
+     bonus = 0.13;
+    }
+  
+  
   newArray[1] = bonus;
   newArray[2] = baseSalary * (1.0 + bonus);
   newArray[3] = Math.round(baseSalary * bonus);
@@ -63,12 +76,12 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+  return basePercent;
 }
 
-function getYearAdjustment(employeeNumber){
+function getYearAdjustment(employeeNumber,reviewScore){
   var yearAdjustment = 0;
-  if(employeeNumber.length == 4){
+  if(employeeNumber.length == 4 && reviewScore > 2){
     yearAdjustment = 0.05;
   }
   return yearAdjustment;
